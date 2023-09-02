bc5ed857f705f881ed8b95c7f8aa0fadb6dda427
Saowaluck Suksawat <poppy@odds.team>
2023-09-02 16:12:18 +0700

    Refactor by separate file of each lang

--- plugins/i18n.ts
+++ plugins/i18n.ts
@@ -1,18 +1,14 @@
 import { createI18n } from 'vue-i18n'
+import en from '../locales/en.json'
+import ja from '../locales/ja.json'
 export default defineNuxtPlugin(({ vueApp }) => {
     const i18n = createI18n({
         legacy: false,
         globalInjection: true,
         locale: 'en',
         messages: {
-            en: {
-                hello: 'Hello, {name}!',
-                language: "Language"
-            },
-            ja: {
-                hello: "こんにちは、{name}！",
-                language: "言語"
-            }
+            en,
+            ja
         }
     })
 
